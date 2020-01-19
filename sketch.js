90154cfc77baf7484203b64fc66c71631f8082d5
GGD <55097456+GamerguysGreatGames@users.noreply.github.com>
2020-01-19 11:28:37 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -75,7 +75,8 @@ screen.overlap(dirt,e)
 sucker.overlap(dirt,w)
 sucker.shapeColor="yellow"
  fill("black")
- text("Enemies left:"+el,50,50)
+ text("Enemies left:"+el,0,0)
+ textSize(5)
 if (player.y>180){player.y=player.y-15}
 if (el<=0){window.alert("You Won");if(eee==false){}}
 }}
